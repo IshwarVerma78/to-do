aefde74d512654a6cf85643f32e12411ef19d12a
Khushi Choudhary <khushivermaa85@gmail.com>
2026-06-10 10:37:45 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100">
+       <div className="addTodo">
+        <h1>className=</h1>
+
+       </div>
+
+          <h1 className='text-xl font-bold'>Your Todos </h1>
+      <div className="todos">
+        <div className="todo">
+
+        </div>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default App
